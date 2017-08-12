869cfb23f0e28e6300452692ed6e8cd06466e04c
Danilo Feijó <danilofs@ciandt.com>
2017-08-12 10:52:14 -0300

    User section Improvement - keeping user section after each scenario execution

--- invoices/tests/features/support/env.rb
+++ invoices/tests/features/support/env.rb
@@ -1,7 +1,12 @@
 require 'capybara'
-require 'capybara/cucumber'
+# require 'capybara/cucumber' # reset user section after each scenario execution
+require 'capybara/dsl'				# Added to keep user section after scenario executions
+require 'capybara/rspec/matchers'	# Added to keep user section after scenario executions
 require 'selenium-webdriver'
 require 'rspec'
+
+World(Capybara::DSL)				# Added to keep user section after scenario executions
+World(Capybara::RSpecMatchers)		# Added to keep user section after scenario executions
 
 Capybara.configure do |config|
 	config.default_driver = :selenium
