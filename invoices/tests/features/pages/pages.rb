5e2b5123ffbe524dadad1165966d2782240419dd
Danilo Feijó <danilofs@ciandt.com>
2017-08-17 01:17:37 -0300

    Adding page objects to hooks

--- invoices/tests/features/pages/pages.rb
+++ invoices/tests/features/pages/pages.rb
@@ -5,17 +5,18 @@ class HomePage < SitePrism::Page
 end
 
 class LoginPage < SitePrism::Page
+	set_url '/login'
 	element :form, '#login_form'
 	element :username, '#email'
 	element :password, '#password'
 	element :title, '#login_form h5'
 	element :login_button, 'button.login-button[type="submit"]'
-
 	element :alert_message, 'div[class*=alert-warning]'
 end
 
 class NavbarPage < SitePrism::Page
 	element :user_menu, '#menu-item-dropdown a[data-toggle=dropdown]'
+	element :option_sair, 'a[href$=logout]'
 end
 
 class DashboardPage < SitePrism::Page
--- invoices/tests/features/support/hooks.rb
+++ invoices/tests/features/support/hooks.rb
@@ -8,9 +8,16 @@
 # end
 
 After ('@logout') do
-	find('#menu-item-dropdown a[data-toggle=dropdown]').click
-	find('a[href$=logout]').click
-	visit '/login'
+	# Coded without pageObjects
+	# find('#menu-item-dropdown a[data-toggle=dropdown]').click
+	# find('a[href$=logout]').click
+	# visit '/login'
+
+	nav = NavbarPage.new
+	login = LoginPage.new
+	nav.user_menu.click
+	nav.option_sair.click
+	login.load
 end
 
 Before do
