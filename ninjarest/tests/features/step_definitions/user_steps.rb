d2a5972b0a3b0ec8eb0eb1e44311d483bc9a0796
Danilo Feijó <danilofs@ciandt.com>
2017-09-04 16:16:05 -0300

    [api] user validation

--- ninjarest/tests/features/step_definitions/user_steps.rb
+++ ninjarest/tests/features/step_definitions/user_steps.rb
@@ -1,13 +1,27 @@
 
 When(/^I get a query throught user service$/) do
 	@getResult = HTTParty.get('http://ninjarest.herokuapp.com/users')
-	puts @getResult
+	
+	# parameter options
+	# puts @getResult.code	---> Most commom
+	# puts @getResult.message
+	# puts @getResult.headers.inspect
+	# puts @getResult.body 
 end
   
-Then(/^response is "([^"]*)"$/) do |arg1|
-	pending # Write code here that turns the phrase above into concrete actions
+Then(/^response is "([^"]*)"$/) do |status_code|
+	expect(@getResult.response.code).to eql status_code
 end
   
 Then(/^system returns a list of registered users$/) do
-	pending # Write code here that turns the phrase above into concrete actions
+	response = @getResult.parsed_response
+	expect(response).not_to be_empty
+
+	# Check what parameters are available into each object
+	response.each do |user|
+		expect(user).to have_key('id')
+		expect(user).to have_key('name')
+		expect(user).to have_key('email')
+		expect(user).to have_key('password')
+	end
 end
