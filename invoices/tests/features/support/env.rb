e27d0659ce8439bc09106b7684aa3ac994565cce
Danilo Feijo <danilo.silvafs@gmail.com>
2017-09-30 10:19:39 -0300

    Enabled Chrome to execute tests

--- invoices/tests/features/support/env.rb
+++ invoices/tests/features/support/env.rb
@@ -10,8 +10,21 @@ require 'faker'
 World(Capybara::DSL)				# Added to keep user section after scenario executions
 World(Capybara::RSpecMatchers)		# Added to keep user section after scenario executions
 
+# Chromedrive settings - Enabling Chrome browser for automation
+Capybara.register_driver :chrome do |app|
+	options = {
+		:js_errors => false,
+		# :timeout => 360,
+		:debug => false,
+		:inspector => false,
+	}
+	Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+# Choose default browser (Chrome and Firefox)
 Capybara.configure do |config|
-	config.default_driver = :selenium
+	config.default_driver = :chrome
+	# config.default_driver = :selenium
 	config.app_host = 'http://ninjainvoices.herokuapp.com'
 end
 
